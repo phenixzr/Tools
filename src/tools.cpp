8da69c0326ebcfed013f764006987f96158b0fab
olivier roux <olivier roux>
2017-05-15 22:33:03 +0200

    FEA test reading value from LUA

--- src/tools.cpp
+++ src/tools.cpp
@@ -42,49 +42,61 @@ int main(int argc, char** argv)
     tools::Logger::getInstance().init();
     theConfReader.init("Data/config.ini");
 
+#pragma region TEST_LOGGER_+_CONF_READER
 	std::thread t1(dummyFunc,1);
 	std::thread t2(dummyFunc,2);
 	std::thread t3(dummyFunc,3);
 	std::thread t4(dummyFunc,4);
 
 	// test logger
-    LOG_DEBUG("je logue depuis main !");
-    std::this_thread::sleep_for (std::chrono::seconds(1));
-    int x = 13;
-    LOG_DEBUG("je logue une valeur ! %d", x);
+	LOG_DEBUG("je logue depuis main !");
+	std::this_thread::sleep_for (std::chrono::seconds(1));
+	int x = 13;
+	LOG_DEBUG("je logue une valeur ! %d", x);
 
+#pragma region TEST_CONF_READER
 	// test config reader
-    float f = theConfReader.getFloat("angle");
-    std::string str = theConfReader.getStr("title");
-    int i = theConfReader.getInt("speed");
-    
+	float f = theConfReader.getFloat("angle");
+	std::string str = theConfReader.getStr("title");
+	int i = theConfReader.getInt("speed");
 
-    LOG_DEBUG("je recupere la valeur angle  ! %f", f);
-    LOG_DEBUG("je recupere la valeur title  ! %s", str.c_str());
-    LOG_DEBUG("je recupere la valeur speed  ! %d", i);
+	std::cout <<"[ConfReader TEST]\n"<<std::endl;
+	std::cout<<"angle = "<<f<<std::endl;
+	std::cout<<"title = "<<str<<std::endl;
+	std::cout<<"speed = "<<i<<std::endl;  
+#pragma endregion
 
-    t1.join();t2.join();t3.join();t4.join();
+
+	LOG_DEBUG("je recupere la valeur angle  ! %f", f);
+	LOG_DEBUG("je recupere la valeur title  ! %s", str.c_str());
+	LOG_DEBUG("je recupere la valeur speed  ! %d", i);
+
+	t1.join();t2.join();t3.join();t4.join();  
+#pragma endregion
+
 
     // test lua bridge
-	const char* file = "Data/script.lua"; 
+	const char* file1 = "Data/script1.lua"; 
+	const char* file2 = "Data/script2.lua"; 
 	const char* keyStr = "testString";
-	const char* keyNbr = "number";
+	const char* keyInt = "n1";
+	const char* keyDouble = "d1";
+	const char* keyFloat = "f1";
 
-	lua_State* L = luaL_newstate();
-	luaL_dofile(L, file);
-	luaL_openlibs(L);
-	lua_pcall(L, 0, 0, 0);
+	std::cout <<"\n[LuaBridge TEST]\n"<<std::endl;
+	tools::LuaBridgeWrapper reader(file1);
+	std::cout << reader.getValue<std::string>(keyStr) <<std::endl;
+	std::cout << reader.getValue<float>(keyFloat) <<std::endl;
+	std::cout << reader.getValue<double>(keyDouble) <<std::endl;
+	std::cout << reader.getValue<int>(keyInt) <<std::endl;
 
-	luabridge::LuaRef s = luabridge::getGlobal(L, keyStr);
-	luabridge::LuaRef n = luabridge::getGlobal(L, keyNbr);
+	reader.init(file2);
+	std::cout << reader.getValue<std::string>(keyStr) <<std::endl;
+	std::cout << reader.getValue<float>(keyFloat) <<std::endl;
+	std::cout << reader.getValue<double>(keyDouble) <<std::endl;
+	std::cout << reader.getValue<int>(keyInt) <<std::endl;
 
-	std::string luaString = s.cast<std::string>();
-	int answer = n.cast<int>();
-
-	LOG_DEBUG("voici une string LUA %s", luaString.c_str());
-	LOG_DEBUG("voici un nombre LUA %d", answer);
-
-	// test object pool
+#pragma region TEST_OBJPOOL
 	tools::ObjectPool<DummyClass>* dummyObjPool = new tools::ObjectPool<DummyClass>(2,5);
 	DummyClass* dobj = dummyObjPool->grab();
 	dummyObjPool->grab();
@@ -92,7 +104,9 @@ int main(int argc, char** argv)
 	dummyObjPool->grab();
 	dummyObjPool->release(dobj);
 	dobj = dummyObjPool->grab();
-	delete dummyObjPool;
+	delete dummyObjPool;  
+#pragma endregion
+
 
 	sf::RenderWindow window(sf::VideoMode(300, 300), "SFML test!");
     sf::CircleShape shape(150.f);
